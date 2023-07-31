85aef39b56ed8dcc406d65e5e6d6b2f59302b5d0
Lev Zharikov <lev_zharikov@outlook.com>
2023-07-31 13:00:43 +0300

    Added additional comments to KendoClients.cs

--- src/KendoEmailAppSharp/KendoClient.cs
+++ src/KendoEmailAppSharp/KendoClient.cs
@@ -10,6 +10,13 @@ public partial class KendoClient : JsonRestApi, IKendo
         this.apiKey = apiKey;
     }
 
+    /// <summary>
+    /// Adds the "apikey" query parameter to the given path and query parameters dictionary.
+    /// If the query parameters dictionary is null, creates a new dictionary with the "apikey" parameter.
+    /// </summary>
+    /// <param name="path">The path to add the query parameters to.</param>
+    /// <param name="queryParams">The dictionary of query parameters to add the "apikey" parameter to.</param>
+    /// <returns>The path with the added "apikey" query parameter.</returns>
     protected override string AddQueryParams(string path, Dictionary<string, string>? queryParams = null)
     {
         if (queryParams is not null)
